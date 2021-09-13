ce7991dc0a5ab3a54595bc44bad65452d6133668
ajoalex012 <ajoalex012@gmail.com>
2021-09-13 17:31:35 +0530

    buttons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,23 @@
 import React, { useState } from 'react'
 
 function Home() {
-    const [width, setWidth] = useState("600")
+    const [width, setWidth] = useState("576")
     return (
         <div className="my-20">
            <div className="container mx-auto  border border-gray-300 rounded-sm ">
                 <div className="top-bar flex flex-row justify-between p-4">
-                    <div className="flex space-x-4">
-                        <button onClick={()=>{setWidth("360")}}>xs</button>
-                        <button onClick={()=>{setWidth("576")}}>sm</button>
-                        <button onClick={()=>{setWidth("768")}}>md</button>
-                        <button onClick={()=>{setWidth("992")}}>lg</button>
-                        <button onClick={()=>{setWidth("1200")}}>xl</button>
+                    <div className="flex space-x-8 text-sm">
+                        <button onClick={()=>{setWidth("360")}}><i class="ri-cellphone-line text-xl"></i> <br />xs</button>
+                        <button onClick={()=>{setWidth("576")}}><i class="ri-smartphone-line text-xl"></i> <br /> sm</button>
+                        <button onClick={()=>{setWidth("768")}}><i class="ri-tablet-line text-xl"></i> <br /> md</button>
+                        <button onClick={()=>{setWidth("992")}}><i class="ri-macbook-line text-xl"></i> <br /> lg</button>
+                        <button onClick={()=>{setWidth("1200")}}><i class="ri-mac-line text-xl"></i> <br /> xl</button>
                     </div>
-                    <div>
-                        hd
+                    <div className="flex space-x-4 text-sm">
+                        <button className="flex flex-row items-center border px-4 border-gray-200 hover:shadow-lg"><i class="ri-fullscreen-exit-fill"></i> &nbsp; Full screen preview</button>
+                        <button className="flex flex-row items-center border px-4 border-gray-200 hover:shadow-lg"><i class="ri-download-line"></i> &nbsp; Download</button>
+                        <button className="flex flex-row items-center border px-4 border-gray-200 hover:shadow-lg"><i class="ri-code-view"></i> &nbsp; Source Code</button>
+                        <button className="flex flex-row items-center border px-4 border-gray-200 hover:shadow-lg"><i class="ri-file-copy-line"></i></button>
                     </div>
                 </div>
                 <div className="h-96 bg-gray-200 overflow-x-auto">
